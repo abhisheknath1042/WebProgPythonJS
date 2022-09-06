f41a60d6b862fddd2e4744d3654acd6c7cb469b4
Abhishek Nath <abhisheknath1042@gmail.com>
2022-09-06 19:28:04 +0530

    Python Tutorial

---
+++ PythonTut/factorial.py
@@ -0,0 +1,12 @@
+def fact(n):
+    f=1
+    for i in range(1,n+1):
+        f= f*i
+
+    return f
+
+
+x = int(input("Enter a number: "))
+result = fact(x)
+
+print("Factorial of {} is {}".format(x,result))
