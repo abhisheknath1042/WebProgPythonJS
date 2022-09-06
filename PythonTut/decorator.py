b00083b96dbdb4b905d7c3ebf7f6c47ac4f760d4
Abhishek Nath <abhisheknath1042@gmail.com>
2022-09-06 23:16:29 +0530

    Updated Decorator

--- PythonTut/decorator.py
+++ PythonTut/decorator.py
@@ -1,12 +1,28 @@
-def announce(f):
-	def wrapper():
-		print("About to run the function F")
-		f()
-		print("Done with the function")
-	return wrapper
+# def announce(f):
+# 	def wrapper():
+# 		print("About to run the function F")
+# 		f()
+# 		print("Done with the function")
+# 	return wrapper
 
-@announce
-def hello():
-	print("Hello, World!")
+# @announce
+# def hello():
+# 	print("Hello, World!")
 
-hello()
+# hello()
+
+def div(a,b):
+    print(a/b)
+
+def smart_Div(func):
+
+    def inner(a,b):
+        if a<b:
+            a,b = b,a
+        return func(a,b)
+
+    return inner
+
+
+div= smart_Div(div)
+div(2,4)
