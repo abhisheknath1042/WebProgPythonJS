a5d03ebb183582509aca69dbbae9d68247ba955f
Abhishek Nath <abhisheknath1042@gmail.com>
2022-09-07 13:29:32 +0530

    Python Module Tutorial

---
+++ PythonTut/moduleFormula.py
@@ -0,0 +1,13 @@
+#Functions for main file
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
---
+++ PythonTut/modulePython.py
@@ -0,0 +1,8 @@
+from moduleFormula import *
+
+a=9
+b=4
+
+c = mult(a,b)
+
+print(c)
